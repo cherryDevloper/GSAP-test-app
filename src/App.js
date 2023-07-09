bfcb4188a337b2975e88691fd05b8db9075e12a8
sharareDev <sharareshaddev@gmail.com>
2023-07-09 15:09:55 +0330

    implement circle loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import CircleLoader from './components/CircleLoader';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="hero">
+        <CircleLoader />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CircleLoader.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { useRef } from 'react';
+import { gsap } from 'gsap';
+const CircleLoader = () => {
+  const blue = useRef(null);
+  const red = useRef(null);
+  const yellow = useRef(null);
+  const green = useRef(null);
+
+  useEffect(() => {
+    gsap?.fromTo(
+      [blue.current, yellow.current],
+      1,
+      { y: 18 },
+      { y: -18, yoyo: true, repeat: -1 }
+    );
+    gsap?.fromTo(
+      [red.current, green.current],
+      1,
+      { y: -18 },
+      { y: 18, repeat: -1, yoyo: true }
+    );
+  }, []);
+  return (
+    <div>
+      <svg
+        viewBox="0 0 150 33.2"
+        width="180"
+        height="150"
+      >
+        <circle
+          ref={blue}
+          cx="16.1"
+          cy="16.6"
+          r="16.1"
+          fill="#527abd"
+        />
+        <circle
+          ref={red}
+          cx="55.2"
+          cy="16.6"
+          r="16.1"
+          fill="#de4431"
+        />
+        <circle
+          ref={yellow}
+          cx="94.3"
+          cy="16.6"
+          r="16.1"
+          fill="#f4b61a"
+        />
+        <circle
+          ref={green}
+          cx="133.4"
+          cy="16.6"
+          r="16.1"
+          fill="#009e52"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default CircleLoader;
